4cafb00f27fc9ba0371eda3e6b439e2ab3f8e54d
mezz-o <mezzo.dev@gmail.com>
2020-10-30 16:47:26 +0100

    create form and query data from todo repository

--- src/Controller/TodoController.php
+++ src/Controller/TodoController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Todo;
+use App\Form\TodoType;
 use App\Repository\TodoRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -11,15 +14,30 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class TodoController extends AbstractController
 {
     /**
-     * @Route("/", name="app_home")
+     * @Route("/", name="app_home", methods="GET|POST")
      */
-    public function index(TodoRepository $todo): Response
-    {
+    public function index(
+        Request $req,
+        EntityManagerInterface $em,
+        TodoRepository $task
+    ): Response {
+        //Query form
+        $todo = new Todo();
+        $form = $this->createForm(TodoType::class, $todo);
+        $form->handleRequest($req);
 
-        $todos= $todo->findAll();
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($todo);
+            $em->flush();
 
-        return $this->render('todo/index.html.twig', compact('todos') );
+            $this->addFlash('success', 'New task created!');
+        }
+        //Query data
+        $todos = $task->findAll();
+
+        return $this->render('todo/index.html.twig', [
+            'todos' => $todos,
+            'form' => $form->createView(),
+        ]);
     }
-
-
 }
